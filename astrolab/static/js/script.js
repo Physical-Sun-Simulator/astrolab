9845473d732692f8d7671ea1551c44a4c7edbf61
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2024-06-21 17:34:25 +0200

    Partial UI update

--- astrolab/static/js/script.js
+++ astrolab/static/js/script.js
@@ -1,5 +1,19 @@
+const apiURLBase = "https://nominatim.openstreetmap.org/search?q=";
+const menuWidth = "400px"
+const baseLatitude = 51.4486602;
+const baseLongitude = 5.4903995655080475;
+const baseZoomLevel = 17;
 var openMenu = false;
-var menuWidth = "400px"
+var map = L.map('map').setView([baseLatitude, baseLongitude], baseZoomLevel);
+L.marker([baseLatitude, baseLongitude]).addTo(map);
+setupMap();
+
+function setupMap() {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
 
 function changeMenu() {
     if (!openMenu) { // Menu is closed
@@ -20,4 +34,28 @@ function changeMenu() {
         console.log('Closing menu');
         openMenu = false;
     }
+}
+
+async function getCoordinates() {
+    const parameter = encodeURIComponent(document.getElementById('location-field').value);
+    const response = await fetch(apiURLBase + parameter + "&format=json&limit=1");
+    const results = await response.json();
+    const target = results[0];
+
+    document.getElementById("longitude-field").value = target.lon;
+    document.getElementById("latitude-field").value = target.lat;
+
+    setMap(target.lat, target.lon);
+
+    console.log(target);
+}
+
+function setMap(latitude, longitude) {
+    map.setView([latitude, longitude], baseZoomLevel);
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     maxZoom: 20,
+    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // }).addTo(map);
+
+    var marker = L.marker([latitude, longitude]).addTo(map);
 }
--- astrolab/webserver.py
+++ astrolab/webserver.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from astrolab.user_interface_node import user_interface_node
 from sun_position import get_sun_elevation_angle, get_sun_azimuth_angle, get_hour_angle, get_true_solar_time, get_equation_of_time, get_sun_declination
-import rclpy, math
+import rclpy, math, threading, datetime, sys
 
 app = Flask(__name__)
 
@@ -43,7 +43,6 @@ def index():
 def arm_angle():
     if request.method == 'POST':
         input = float(request.form.getlist('number')[0])
-        rclpy.spin_once(node,timeout_sec=1.0)
         node.arm_send_goal(input)
         return redirect("/")
     
@@ -51,7 +50,6 @@ def arm_angle():
 def arm_speed():
     if request.method == 'POST':
         input = float(request.form.getlist('number')[0])
-        rclpy.spin_once(node,timeout_sec=1.0)
         node.change_arm_speed(input)
         return redirect("/")
     
@@ -59,7 +57,6 @@ def arm_speed():
 def table_angle():
     if request.method == 'POST':
         input = float(request.form.getlist('number')[0])
-        rclpy.spin_once(node,timeout_sec=1.0)
         node.table_send_goal(input)
         return redirect("/")
     
@@ -67,9 +64,23 @@ def table_angle():
 def table_speed():
     if request.method == 'POST':
         input = float(request.form.getlist('number')[0])
-        rclpy.spin_once(node,timeout_sec=1.0)
         node.change_table_speed(input)
         return redirect("/")
+    
+@app.route('/calibrate', methods = ['POST'])
+def calibrate():
+    if request.method == 'POST':
+        if request.form['start'] == 'Start':
+            elevation = request.form['elevation']
+            azimuth = request.form['azimuth']
+            # speed = request.form['speed']
+            node.arm_send_goal(math.radians(float(elevation)))
+            node.table_send_goal(math.radians(float(azimuth)))
+            # node.change_table_speed(float(speed))
+            print("Starting machine", file=sys.stderr)
+        else:
+            print("Stopping machine", file=sys.stderr)
+        return redirect("/calibration")
     
 @app.route('/simulate', methods = ['POST'])
 def calculate():
@@ -84,7 +95,6 @@ def calculate():
         sun_declination = get_sun_declination(day)
         sun_elevation_angle = get_sun_elevation_angle(geographical_latitude, sun_declination, hour_angle)
         sun_azimuth_angle = get_sun_azimuth_angle(sun_elevation_angle, geographical_latitude, sun_declination, true_solar_time)
-        rclpy.spin_once(node,timeout_sec=1.0)
         node.arm_send_goal(math.degrees(sun_elevation_angle))
         node.table_send_goal(math.degrees(sun_azimuth_angle))
         return f"""<h1>Starting temporary algorithm!!!</h1>
@@ -95,71 +105,27 @@ def calculate():
 @app.route('/simulation', methods = ['GET'])
 def simulate():
     if request.method == 'GET':
-        # geographical_latitude = math.radians(float(request.form.getlist('geographical_latitude')[0]))
-        # geographical_longitude = math.radians(float(request.form.getlist('geographical_longitude')[0]))
-        # local_clock_time = float(request.form.getlist('local_clock_time')[0])
-        # day = float(request.form.getlist('day')[0])
-        # equation_of_time = get_equation_of_time(day)
-        # true_solar_time = get_true_solar_time(local_clock_time, geographical_longitude, equation_of_time)
-        # hour_angle = get_hour_angle(true_solar_time)
-        # sun_declination = get_sun_declination(day)
-        # sun_elevation_angle = get_sun_elevation_angle(geographical_latitude, sun_declination, hour_angle)
-        # sun_azimuth_angle = get_sun_azimuth_angle(sun_elevation_angle, geographical_latitude, sun_declination, true_solar_time)
-        # rclpy.spin_once(node,timeout_sec=1.0)
-        # node.arm_send_goal(math.degrees(sun_elevation_angle))
-        # node.table_send_goal(math.degrees(sun_azimuth_angle))
-        # return f"""<h1>Starting temporary algorithm!!!</h1>
-        #             <h2>Calculated Angles:</h2>
-        #             <h3>Sun Elevation Angle (Arm) = {math.degrees(sun_elevation_angle)}</h3>
-        #             <h3>Sun Azimuth Angle (Table) = {math.degrees(sun_azimuth_angle)}</h3>"""
         return render_template('simulation.html', title=options[0], name=name, options=options)
     
 @app.route('/calibration', methods = ['GET'])
 def calibration():
     if request.method == 'GET':
-        # geographical_latitude = math.radians(float(request.form.getlist('geographical_latitude')[0]))
-        # geographical_longitude = math.radians(float(request.form.getlist('geographical_longitude')[0]))
-        # local_clock_time = float(request.form.getlist('local_clock_time')[0])
-        # day = float(request.form.getlist('day')[0])
-        # equation_of_time = get_equation_of_time(day)
-        # true_solar_time = get_true_solar_time(local_clock_time, geographical_longitude, equation_of_time)
-        # hour_angle = get_hour_angle(true_solar_time)
-        # sun_declination = get_sun_declination(day)
-        # sun_elevation_angle = get_sun_elevation_angle(geographical_latitude, sun_declination, hour_angle)
-        # sun_azimuth_angle = get_sun_azimuth_angle(sun_elevation_angle, geographical_latitude, sun_declination, true_solar_time)
-        # rclpy.spin_once(node,timeout_sec=1.0)
-        # node.arm_send_goal(math.degrees(sun_elevation_angle))
-        # node.table_send_goal(math.degrees(sun_azimuth_angle))
-        # return f"""<h1>Starting temporary algorithm!!!</h1>
-        #             <h2>Calculated Angles:</h2>
-        #             <h3>Sun Elevation Angle (Arm) = {math.degrees(sun_elevation_angle)}</h3>
-        #             <h3>Sun Azimuth Angle (Table) = {math.degrees(sun_azimuth_angle)}</h3>"""
         return render_template('calibration.html', title=options[1], name=name, options=options)
     
 @app.route('/dynamics', methods = ['GET'])
 def dynamics():
     if request.method == 'GET':
-        # geographical_latitude = math.radians(float(request.form.getlist('geographical_latitude')[0]))
-        # geographical_longitude = math.radians(float(request.form.getlist('geographical_longitude')[0]))
-        # local_clock_time = float(request.form.getlist('local_clock_time')[0])
-        # day = float(request.form.getlist('day')[0])
-        # equation_of_time = get_equation_of_time(day)
-        # true_solar_time = get_true_solar_time(local_clock_time, geographical_longitude, equation_of_time)
-        # hour_angle = get_hour_angle(true_solar_time)
-        # sun_declination = get_sun_declination(day)
-        # sun_elevation_angle = get_sun_elevation_angle(geographical_latitude, sun_declination, hour_angle)
-        # sun_azimuth_angle = get_sun_azimuth_angle(sun_elevation_angle, geographical_latitude, sun_declination, true_solar_time)
-        # rclpy.spin_once(node,timeout_sec=1.0)
-        # node.arm_send_goal(math.degrees(sun_elevation_angle))
-        # node.table_send_goal(math.degrees(sun_azimuth_angle))
-        # return f"""<h1>Starting temporary algorithm!!!</h1>
-        #             <h2>Calculated Angles:</h2>
-        #             <h3>Sun Elevation Angle (Arm) = {math.degrees(sun_elevation_angle)}</h3>
-        #             <h3>Sun Azimuth Angle (Table) = {math.degrees(sun_azimuth_angle)}</h3>"""
         return render_template('dynamics.html', title=options[2], name=name, options=options)
     
 def main():
-    app.run(debug=True)
+    t1 = threading.Thread(target=app.run(debug=True), name='t1')
+    t2 = threading.Thread(target=rclpy.spin(node), name='t2')
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
 
 if __name__ == '__main__':
     main()
